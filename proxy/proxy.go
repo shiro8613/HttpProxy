afc4cf82a0f8f48ddb7faa9e84666bc506b73499
toms0910 <psp1070@gmail.com>
2023-05-31 01:40:16 +0900

    delete bool

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -7,19 +7,18 @@ import (
 
 func Proxy(handlers Proxys) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var bo bool = false
 		for k, v := range handlers {
 			if strings.Contains(r.URL.Path, k) {
-				bo = true
 				if r.URL.Path == k && r.URL.Path != (k + "/") {
 					http.Redirect(w, r, k + "/", http.StatusFound)
+　　　return
 				}
 				v.ServeHTTP(w, r)
+    return
 			}
 		}
 
-		if !bo {
 			w.WriteHeader(http.StatusNotFound)
-		}
+		
 	})
 }
